29666b3c22e2408090b100f0731ddaf7b3cc698b
Holly Springsteen <hhspringsteen@gmail.com>
2016-05-01 23:25:49 -0500

    tracker for SPA layout

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,27 @@ $(function(){
     var obj = $(this);
     pageFlow(e, obj);
   });
+
+
+
+  ////////////////////////////// TRACKER
+  //////////////////////////////////////
+  function tracker(){
+    var scrollTop = $(window).scrollTop();
+
+    $('.tracker-section').each(function(){
+      var top = $(this).position().top;
+      var id = $(this).attr('id');
+
+      if(top < scrollTop + 50){
+        $('.tracker-link[href="#'+id+'"').addClass('tracker-current');
+      }else{
+        $('.tracker-link[href="#'+id+'"').removeClass('tracker-current');
+      }
+    });
+  }
+
+  $(window).scroll(function(){
+    tracker();
+  });
 });
